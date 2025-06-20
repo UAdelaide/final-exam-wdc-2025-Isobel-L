b26c3876f4b93bc4c3c055f7df2a1dc0661a1275
Isobel.l <Isobel.leviton@gmail.com>
2025-06-20 07:21:50 +0000

    [automatic commit] 2025-06-20 07:21:50 UTC 49.178.198.125

--- part1/app.js
+++ part1/app.js
@@ -19,7 +19,7 @@ async function initDB(){
 
     console.log('connected to mysql');
     } catch (error) {
-        console.error()
+        console.error('Failed to connect to MySQL:', error);
     }
 
 // route: /api/dogs
