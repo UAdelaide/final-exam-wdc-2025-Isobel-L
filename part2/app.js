d1cf42c91e174c91a60b320a6d3e5f73bb12f5ad
Isobel.l <Isobel.leviton@gmail.com>
2025-06-20 09:54:15 +0000

    [automatic commit] 2025-06-20 09:54:15 UTC 49.178.198.125

--- part2/app.js
+++ part2/app.js
@@ -11,7 +11,8 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(session({
     secret: 'secret_key',
-    resave
+    resave: false,
+    saveUninitalized: true 
 }))
 
 // Routes
