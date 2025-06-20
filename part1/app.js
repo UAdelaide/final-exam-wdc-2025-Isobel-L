5862e3506d3566cef2ec4830fb43e5b82e1b52d3
Isobel.l <Isobel.leviton@gmail.com>
2025-06-20 06:27:52 +0000

    [automatic commit] 2025-06-20 06:27:52 UTC 49.178.198.125

--- part1/app.js
+++ part1/app.js
@@ -29,7 +29,7 @@ INSERT IGNORE INTO Users (username, email, password_hash, role)
     ('isobel124', 'isobel@example.com', 'hashed111', 'owner'),
     ('luffywalker,', 'luffy@example.com', 'hashed222', 'walker');
 
-INSERT IGNORE INTO Dogs (name, ResizeObserverSize, owner_id) VALUES
+INSERT IGNORE INTO Dogs (name, size, owner_id) VALUES
 
 
 
