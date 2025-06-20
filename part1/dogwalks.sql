4be0b21ec520305c80071706ddbf8f3571988338
Isobel.l <Isobel.leviton@gmail.com>
2025-06-20 05:03:31 +0000

    [automatic commit] 2025-06-20 05:03:30 UTC 49.178.198.125

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -56,4 +56,4 @@ CREATE TABLE WalkRatings (
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
 VALUES
-((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2'
+((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', '30'
