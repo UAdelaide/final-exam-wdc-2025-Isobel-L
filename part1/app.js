402699543a84ddea06764538fa7afe9003364533
Isobel.l <Isobel.leviton@gmail.com>
2025-06-20 06:06:35 +0000

    [automatic commit] 2025-06-20 06:06:35 UTC 49.178.198.125

--- part1/app.js
+++ part1/app.js
@@ -55,5 +55,6 @@ app.get('/api/walkers/summary,', async (req, res) => {
         const [rows] = await db.query(`
             SELECT
             u.username as walker_username,
-            COUNT(r.rating)
+            COUNT(r.rating) AS total_ratings,
+            ROUND(AVG(r.rating), 1)
 
