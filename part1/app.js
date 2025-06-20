258519a698102308819efcf433e29178481add8a
Isobel.l <Isobel.leviton@gmail.com>
2025-06-20 05:47:42 +0000

    [automatic commit] 2025-06-20 05:47:42 UTC 49.178.198.125

--- part1/app.js
+++ part1/app.js
@@ -23,7 +23,8 @@ app.get('/api/dogs', async (req, res) => {
         );
         res.json(rows);
         } catch (error) {
-         res.status(500).json({ error: 'Failed to })
+         res.status(500).json({ error: 'Failed to fetch dogs :(' });
+         
 
         }
 
