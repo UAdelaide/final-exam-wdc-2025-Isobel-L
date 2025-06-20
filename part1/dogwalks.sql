f5c9943af34be7544d3d7c671140d1f5b2103a61
Isobel.l <Isobel.leviton@gmail.com>
2025-06-20 05:07:50 +0000

    [automatic commit] 2025-06-20 05:07:50 UTC 49.178.198.125

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -59,4 +59,5 @@ VALUES
 ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands','open'),
 ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
 ((SELECT dog_id FROM Dogs WHERE name = 'Charlie'), '2025-06-11 10:00:00', 60, 'Riverside Park', 'open'),
-((SELECT dog_id FROM Dogs WHERE name = 'Daisy'), '2025-06-12 15:00:00, 20, 'City Gardens', 'open'), 
+((SELECT dog_id FROM Dogs WHERE name = 'Daisy'), '2025-06-12 15:00:00', 20, 'City Gardens', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'Rocky'
