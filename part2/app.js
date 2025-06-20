82ef6dbf59d3b291ed218d74cbf0603ec5d4b730
Isobel.l <Isobel.leviton@gmail.com>
2025-06-20 10:30:23 +0000

    [automatic commit] 2025-06-20 10:30:23 UTC 49.178.198.125

--- part2/app.js
+++ part2/app.js
@@ -33,33 +33,7 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
-app.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-
-    try {
-        const [rows] = await db.query(
-            'SELECT * FROM Users WHERE username = ?',
-            [username]
-        );
-
-        if (rows.length === 0){
-            return res.status(401).json({error: "Invalid username or password :("});
-        }
-
-        const user = rows[0];
-        if (password !== user.password_hash){
-            return res.status(401).json({ error: 'Invalid username or password :('})
-        }
-
-        req.session.userId = user.user_id;
-        req.session.role = user.role;
-
-        res.json({message: 'Login successful :)', role: user.role });
-    } catch (error) {
-            console.error('Login error:', error);
-            res.status(500).json({ error: 'Internal server error'});
-        }
-    );
+app.post('/login', async (req, res) => {});
 
 app.post('/logout', (req, res) => {
     req.session.destroy(() => {
