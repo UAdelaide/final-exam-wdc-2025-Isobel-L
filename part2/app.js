4f734b6482e43b31d4630873eef99757658849d6
Isobel.l <Isobel.leviton@gmail.com>
2025-06-20 12:33:31 +0000

    [automatic commit] 2025-06-20 12:33:31 UTC 122.108.145.96

--- part2/app.js
+++ part2/app.js
@@ -36,6 +36,8 @@ app.use('/api/users', userRoutes);
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
 
+    document.write("Hi Isobel");
+    
     try {
         const [rows] = await db.query(
             'SELECT * FROM Users WHERE username = ?',
