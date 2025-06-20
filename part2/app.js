5511a93fcf2aac638c99e717c7148d9541890126
Isobel.l <Isobel.leviton@gmail.com>
2025-06-20 09:59:45 +0000

    [automatic commit] 2025-06-20 09:59:44 UTC ;; communications error to 208.67.222.222#53: timed out 49.178.198.125

--- part2/app.js
+++ part2/app.js
@@ -33,7 +33,7 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
-app.post()
+app.post('/login', async (this.request, res))
 
 // Export the app instead of listening here
 module.exports = app;
