bc26e20f52bc893a2d7ee8cfb33dbc4f863924fc
Isobel.l <Isobel.leviton@gmail.com>
2025-06-20 05:14:31 +0000

    [automatic commit] 2025-06-20 05:14:31 UTC 49.178.198.125

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -58,7 +58,8 @@ INSERT INTO Users (username, email, password_hash, role)
 VALUES
 ('alice123', 'alice@example.com', 'hashed123'),
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-('carol123', 'carol@example.com')
+('carol123', 'carol@example.com', 'hashed789', 'owner'),
+('davey')
 
 
 
