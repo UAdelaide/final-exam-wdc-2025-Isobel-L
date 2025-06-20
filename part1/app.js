e685b7b990cab5d2c02dc8b4c6cac33859c3c652
Isobel.l <Isobel.leviton@gmail.com>
2025-06-20 05:49:09 +0000

    [automatic commit] 2025-06-20 05:49:09 UTC 49.178.198.125

--- part1/app.js
+++ part1/app.js
@@ -13,14 +13,14 @@ let db;
 // route: /api/dogs
 app.get('/api/dogs', async (req, res) => {
     try {
-        const[rows] = await db.query((
+        const[rows] = await db.query(`
             SELECT
             Dogs.name AS dog_name,
             Dogs.size,
             Users.username AS owner_username
             FROM Dogs
             Join Users ON Dogs.owner_id = Users.id;
-        );
+        `);
         res.json(rows);
         } catch (error) {
          res.status(500).json({ error: 'Failed to fetch dogs :(' });
