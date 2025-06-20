93b45c682d5cb497a199d67b43d63bac11267df1
Isobel.l <Isobel.leviton@gmail.com>
2025-06-20 06:10:41 +0000

    [automatic commit] 2025-06-20 06:10:41 UTC 49.178.198.125

--- part1/app.js
+++ part1/app.js
@@ -65,5 +65,5 @@ app.get('/api/walkers/summary,', async (req, res) => {
             `);
             res.json(rows);
         } catch (error){
-            res.status(500.json({ error: 'Failed to fetch '
+            res.status(500.json({ error: 'Failed to fetch walker summary :('})
 
