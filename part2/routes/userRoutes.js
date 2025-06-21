a9748e5aaf5ee6c9e9e05867fa39839f49d5ff2a
Isobel.l <Isobel.leviton@gmail.com>
2025-06-21 00:38:40 +0000

    [automatic commit] 2025-06-21 00:38:40 UTC 122.108.145.96

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -70,7 +70,8 @@ router.get('/dogs', async (req, res) => {
 
       res.json(rows);
     } catch (error){
-      res.status(500)
+      res.status(500).json({ error: 'Failed to fetch dogs' });
+      
     }
   }
 })
