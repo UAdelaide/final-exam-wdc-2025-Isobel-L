52dac5764271f80c87e7db1b56b5e2f30f45a445
Isobel.l <Isobel.leviton@gmail.com>
2025-06-21 03:34:23 +0000

    [automatic commit] 2025-06-21 03:34:23 UTC 122.108.145.96

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -72,7 +72,7 @@ router.get('/dogs', async (req, res) => {
     const [rows] = await db.query(`
       // SELECT dog_id, name FROM Dogs WHERE owner_id = ?
       // `, [ownerId]);
-      SELECT dog_id
+      SELECT dog_id, name, 
 
     res.json(rows);
   } catch (error) {
