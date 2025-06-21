237e19e9ccfcf88867f50a83b0616b58834361cd
Isobel.l <Isobel.leviton@gmail.com>
2025-06-21 00:16:28 +0000

    [automatic commit] 2025-06-21 00:16:27 UTC 122.108.145.96

--- part2/app.js
+++ part2/app.js
@@ -63,7 +63,7 @@ app.post('/login', async (req, res) => {
 
 app.post('/logout', (req, res) => {
     req.session.destroy(err => {
-        if (err)
+        if (err) return res.status(500).json({ error: ''})
     }
 
 // Export the app instead of listening here
